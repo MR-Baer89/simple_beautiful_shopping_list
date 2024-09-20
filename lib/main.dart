73af7b18c77c26561e7f935dc92f8675a3c2b48d
Blackbaer89 <d-mueller29@t-online.de>
2024-09-20 16:32:20 +0200

    implement more products

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,13 @@ void main() {
     Product("Apfel", 7.99),
     Product("Wassermelone", 34.99),
     Product("Tee", 12.99),
-    Product("Lego", 649.99)
+    Product("Lego", 649.99),
+    Product("Monster", 1.49),
+    Product("fanta", 2.99),
+    Product("Bananen", 0.99),
+    Product("Pizza", 3.29),
+    Product("Annanas", 1.99),
+    Product("Milch", 0.79)
   ];
 
   runApp(MainApp(products: products));
